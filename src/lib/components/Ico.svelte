9701b4ce68c275cf9429e8b7ec05862c8a2a0a96
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-16 15:43:39 +0530

    Created: Icons Component

---
+++ src/lib/components/Ico.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Icons } from '$data/Icons';
+
+	let {
+		name,
+		class: className,
+		...restProps
+	} = $props<{
+		name: keyof typeof Icons;
+		class?: string;
+		[key: string]: any;
+	}>();
+
+	let displayIcon = $derived(Icons[name]);
+</script>
+
+{#if displayIcon}
+	<svg
+		class={className}
+		xmlns="http://www.w3.org/2000/svg"
+		width="100%"
+		height="100%"
+		viewBox={displayIcon.viewBox}
+		{...restProps}
+	>
+		{@html displayIcon.svg}
+	</svg>
+{/if}
