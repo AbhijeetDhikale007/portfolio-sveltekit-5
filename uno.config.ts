10d3d95a850228dcd804e9ef669cc9e4109ddb02
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-16 14:12:55 +0530

    Created: UnoCSS Config File

---
+++ uno.config.ts
@@ -0,0 +1,44 @@
+import {
+    defineConfig,
+    presetAttributify,
+    presetIcons,
+    presetTypography,
+    presetWind4,
+    presetWebFonts,
+    transformerDirectives,
+    transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+    shortcuts: [
+        // ...
+    ],
+    theme: {
+        colors: {
+            // ...
+        }
+    },
+    presets: [
+        presetWind4(),
+        presetAttributify(),
+        presetIcons(),
+        presetTypography(),
+        presetWebFonts({
+            fonts: {
+                // ...
+                Roboto: {
+                    name: 'Roboto Condensed',
+                    weights: ['300', '400', '500', '700', '800']
+                },
+                Arsenal: {
+                    name: 'Arsenal SC',
+                    weights: ['800']
+                }
+            },
+        }),
+    ],
+    transformers: [
+        transformerDirectives(),
+        transformerVariantGroup(),
+    ],
+})
