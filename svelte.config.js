49709dce0a8ad6459dc08eb1f0a18afa7cc71856
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-16 14:37:09 +0530

    Added: Alias

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,20 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$data: 'src/data',
+			$store: 'src/Store/Store.js',
+			$components: 'src/lib/components'
+		},
+		paths: {
+			relative: true
+		},
+		prerender: {
+			handleHttpError: 'warn'
+		}
+	}
 };
 
 export default config;
