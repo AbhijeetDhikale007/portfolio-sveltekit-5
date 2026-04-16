96c9811cf1b5b01217e8b60dff145f25e542c3a7
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-16 18:29:44 +0530

    Created: Home Social Links Icons Component

---
+++ src/lib/components/HomeIcons.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Ico from '$components/Ico.svelte';
+
+	const SocialLinks = [
+		{ name: 'GitHubW', href: 'https://github.com/AbhijeetDhikale007' },
+		{ name: 'Instagram', href: 'https://instagram.com/abhijeetdhikale007' },
+		{ name: 'LinkedIn', href: 'https://www.linkedin.com/in/abhijeet-dhikale' },
+		{ name: 'Twitter', href: 'https://x.com/abhijeetdhikale' },
+		{ name: 'Telegram', href: 'https://t.me/AbhijeetDhikale007' }
+	];
+</script>
+
+<div class="flex gap-2 rounded-8 overflow-hidden">
+	{#each SocialLinks as link}
+		<div class="bg-white h-10 rounded-2 p-2">
+			<a
+				href={link.href}
+				class="Tooltip invert"
+				data-title={link.name}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<Ico class="Ico" name={link.name} />
+			</a>
+		</div>
+	{/each}
+</div>
