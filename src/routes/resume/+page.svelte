1a3288637ae43cb6500c7e03e1e9d0a1fb817843
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-17 13:52:30 +0530

    Created: Resume Page

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	import { cubicIn } from 'svelte/easing';
+	import Heading from '$components/Heading.svelte';
+
+	let HeadingTitle = $state('Resume');
+</script>
+
+<div
+	class="Resume flex flex-col items-center gap-6;"
+	in:fly={{ easing: cubicIn, y: -100, duration: 1200 }}
+>
+	<Heading {HeadingTitle} />
+	<div class="flex gap-8">
+		<a
+			class="Button-white"
+			href="Documents/Abhijeet-Prakash-Dhikale-Resume.pdf"
+			target="_blank"
+			type="file/pdf">View</a
+		>
+		<a
+			class="Button-white"
+			href="Documents/Abhijeet-Prakash-Dhikale-Resume.pdf"
+			target="_blank"
+			type="file/pdf"
+			download>Download</a
+		>
+	</div>
+	<div
+		class="mt-4 mb-8 grow-1 shrink-1 w-[80vw] h-[56vh] md:w-[76vw] md:h-48 lg:w-[76vw] lg:h-[48vh] xl:w-[76vw] xl:h-[48vh] 2xl:w-[76vw] xl:h-[48vh]"
+	>
+		<!-- <iframe
+			class="w-full h-full border-none"
+			src="/Documents/Abhijeet-Prakash-Dhikale-Resume.pdf#view=FitH"
+			title="Resume PDF"
+			loading="lazy"
+		></iframe> -->
+	</div>
+</div>
+
+<style lang="scss">
+	.Resume {
+		.Button-white {
+			padding: 8px 14px;
+			transition: 0.5s;
+			@apply bg-white border-none rounded-2 text-4.1 cursor-pointer text-black font-500 no-underline select-none;
+		}
+
+		.Button-white:hover {
+			scale: 1.2;
+		}
+
+		.Button-white:focus {
+			color: rgb(0, 250, 250);
+			border-bottom: 0.5px solid rgb(0, 250, 250);
+		}
+	}
+</style>
