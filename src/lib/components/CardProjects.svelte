84f9ceacc78680a9c4a3ae98549183ea579ca2ca
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-17 16:53:14 +0530

    Many Changes or Adjustments

--- src/lib/components/CardProjects.svelte
+++ src/lib/components/CardProjects.svelte
@@ -27,54 +27,59 @@
 </script>
 
 {#each Project as project}
-	<div class="CardProject" use:tilt>
-		<div class="CP0">
+	<div
+		class="border-1 border-black/40 hover:border-black dark:border-white/40 dark:hover:border-white w-full max-w-[400px] lg:w-[30vw] xl:w-[26vw] bg-transparent flex flex-wrap flex-col gap-y-3 rounded-xl p-6 transition-colors"
+		use:tilt
+	>
+		<div class="w-full">
 			<Ico class="w-15 h-15" name={project.Logo} />
 		</div>
-		<div class="CP1">
-			<h2>{project.Title}</h2>
-			<div class="CP1Div">
+		<div class="flex justify-between items-center w-full">
+			<h2 class="text-2xl font-semibold">{project.Title}</h2>
+			<div class="flex items-center gap-x-2">
 				<a
-					class="Tooltip w-5 h-5 flex content-center"
+					class="Tooltip w-8 h-8 p-1.2 flex content-center border-1 border-black/40 hover:border-black dark:border-white/40 dark:hover:border-black rounded-2 transition-all bg-black hover:bg-black hover:invert dark:bg-black dark:invert dark:hover:invert-0"
 					data-title="GitHub"
 					href={project.urlGit}
 					target="_blank"
 				>
-					<Ico class="h-6 w-6" name="GitHubW" />
+					<Ico name="GitHubW" />
 				</a>
 				<a
-					class="Tooltip w-5 h-5 flex content-center"
+					class="Tooltip w-8 h-8 p-1.2 flex content-center border-1 border-white/40 hover:border-black dark:hover:border-black rounded-2 transition-all bg-black hover:bg-black hover:invert dark:bg-black dark:invert dark:hover:invert-0"
 					data-title="Deployment"
 					href={project.urlDeploy}
 					target="_blank"
 				>
-					<Ico class="h-6 w-6" name="Link" />
+					<Ico name="Link" />
 				</a>
 			</div>
 		</div>
 		<hr />
-		<div class="CP2">
+		<div class="flex gap-3 items-center w-full font-medium text-sm">
 			<Ico name="Project" class="h-6 w-6" />
 			<h4>{project.Type}</h4>
 		</div>
 		<hr />
-		<div class="CP3">
+		<div class="flex gap-3 items-center w-full font-medium text-sm">
 			<Ico name="Time" class="h-6 w-6" />
 			<h4>{project.Duration}</h4>
 		</div>
 		<hr />
-		<div class="CP4">
-			<h4 class="font-light">{project.Details}</h4>
+		<div class="flex flex-grow flex-shrink py-2 min-h-[14vh]">
+			<h4 class="text-sm leading-relaxed text-justify m-0 opacity-80 font-light">
+				{project.Details}
+			</h4>
 		</div>
-		<div class="CP5">
-			<div><p>{project.From}</p></div>
-			<div><p>{project.To}</p></div>
+		<div class="flex justify-between items-center w-full text-xs font-semibold">
+			<div class="px-3 py-2 border-1 border-white/20 rounded-full"><p>{project.From}</p></div>
+			<div class="px-3 py-2 border-1 border-white/20 rounded-full"><p>{project.To}</p></div>
 		</div>
 		<hr />
-		<div class="flex gap-x-2 items-center justify-start h-[10vh] w-100%;">
+		<div class="flex gap-x-2 items-center justify-start h-[6vh] w-full mt-auto">
 			{#each [project.SVG1, project.SVG2, project.SVG3, project.SVG4, project.SVG5, project.SVG6, project.SVG7, project.SVG8, project.SVG9, project.SVG10].filter( (svg): svg is string => Boolean(svg) ) as svg}
 				<div class="Tooltip" data-title={svg}>
-					<Ico class="border-1 border-white w-5 h-5 p-1.6 rounded-3 h-6 w-6" name={svg} />
+					<Ico class="border-1 border-white/40 w-8 h-8 p-1.6 rounded-2" name={svg} />
 				</div>
 			{/each}
 		</div>
@@ -82,74 +87,9 @@
 {/each}
 
 <style lang="scss">
-	.CardProject {
-		border: 0.6px solid rgba(100, 100, 100, 0.3);
-		@apply bg-transparent flex flex-wrap flex-col gap-y-3 rounded-xl p-6 transition-colors;
-		
-		width: 100%;
-		max-width: 400px;
-		@apply lg:w-[30vw] xl:w-[26vw];
-
-		&:hover {
-			border-color: rgba(100, 100, 100, 0.8);
-		}
-	}
-
 	hr {
 		border: none;
-		border-bottom: 0.4px solid rgba(100, 100, 100, 0.3);
+		border-bottom: 0.4px solid rgba(100, 100, 100, 0.5);
 		margin: 0;
-	}
-
-	.CP0 {
-		@apply w-full;
-	}
-
-	.CP1 {
-		@apply flex justify-between items-center w-full;
-
-		h2 {
-			@apply text-2xl font-bold;
-		}
-
-		.CP1Div {
-			@apply flex items-center gap-x-2;
-
-			a {
-				border: 0.4px solid rgba(100, 100, 100, 0.5);
-				@apply flex rounded items-center p-1.5 transition-all hover:bg-black/10 dark:hover:bg-white/10;
-			}
-		}
-	}
-
-	.CP2, .CP3 {
-		@apply flex gap-3 items-center w-full font-medium text-sm;
-		
-		h4 {
-			@apply m-0;
-		}
-	}
-
-	.CP4 {
-		@apply py-2 min-h-[14vh];
-
-		h4 {
-			@apply text-sm leading-relaxed text-justify m-0 opacity-80 font-light;
-		}
-	}
-
-	.CP5 {
-		@apply flex justify-between items-center w-full;
-
-		div {
-			border: 0.5px solid rgba(100, 100, 100, 0.5);
-			@apply py-[2px] px-3 rounded-full text-xs font-semibold;
-			
-			p { @apply m-0; }
-		}
-	}
-
-	.CP6 {
-		@apply flex gap-x-2 items-center justify-start h-[6vh] w-full mt-auto;
 	}
 </style>
