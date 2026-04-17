0a61c3d43d8c2ffdb3f4f0a0fa81e7f6c3eb16c5
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-17 11:26:38 +0530

    Implemented: Lenis Smooth Scrolling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,20 @@
 	import Header from '$components/Header.svelte';
 	import '../app.scss';
 	import 'virtual:uno.css';
+	import { onMount } from 'svelte';
+	import Lenis from 'lenis';
 	import { setTheme, createThemeState } from '$lib/theme.svelte';
 
 	let { children } = $props();
 
 	const theme = createThemeState();
 	setTheme(theme);
+
+	onMount(() => {
+		const lenis = new Lenis({
+			autoRaf: true
+		});
+	});
 </script>
 
 <svelte:head>
