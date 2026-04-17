6b683a7dd645810f2137169693ca94bc9497d847
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-17 11:26:53 +0530

    Removed: Carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import HomeIcons from '$components/HomeIcons.svelte';
 	import Typewriter from '$components/Typewriter.svelte';
-	import Carousel from '$components/Carousel.svelte';
+	// import Carousel from '$components/Carousel.svelte';
+	import { fly } from 'svelte/transition';
+	import { cubicIn } from 'svelte/easing';
 </script>
 
-<div class="flex flex-col items-center justify-center min-h-[90vh] w-full pt-26 overflow-x-hidden">
+<div
+	class="flex flex-col items-center justify-center min-h-[90vh] w-full pt-26 overflow-x-hidden"
+	in:fly={{ easing: cubicIn, y: 100, duration: 1200 }}
+>
 	<div class="flex flex-col items-center justify-center flex-grow w-full px-4 text-center z-10">
 		<h1
 			class="text-5xl md:text-7xl lg:text-8xl font-bold font-Roboto-Condensed mb-4 md:mb-6 text-black dark:text-white transition-colors duration-500 tracking-tighter"
@@ -25,10 +30,10 @@
 
 	<div class="w-full mt-2">
 		<p
-			class="text-center text-xs md:text-sm mb-4 md:mb-4 opacity-60 font-semibold tracking-[0.3em] uppercase text-black dark:text-white transition-colors duration-500"
+			class="text-center text-xs md:text-sm mb-4 md:mb-4 opacity-80 dark:opacity-50 font-semibold tracking-[0.3em] uppercase text-black dark:text-white transition-colors duration-500"
 		>
 			My Tech Stack
 		</p>
-		<Carousel />
+		<!-- <Carousel /> -->
 	</div>
 </div>
