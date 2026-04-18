1e3b417b1eb4ac3ad628abe2257f6c876039f663
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-18 15:28:25 +0530

    Implemented: Light Mode

--- src/lib/components/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard.svelte
@@ -15,46 +15,44 @@
     }
 </script>
 
-{#each Experience as card}
-<div class="{card.CardClass.replace('CardEdu', '').trim()} CardEdu" use:tilt={{ max: 15, speed: 400, glare: true, "max-glare": 0.2 }}>
-    <img class='Logo' src={card.imgUrl} alt={card.imgAlt} />
-    <h2>{card.Title}</h2>
-    <h3 class="pb-2">{card.Company}</h3>
-    <hr />
+{#each Experience as card, index}
+<div class="CardEdu self-center rounded-5 grow-1 shrink-1 h-auto w-[80vw] md:w-[38vw] lg:w-[38vw] px-4 py-4 lg:px-6 lg:py-6 bg-white dark:bg-black border-1 border-black/10 dark:border-white/10 {index % 2 === 0 ? 'md:self-start lg:self-start' : 'md:self-end lg:self-end'}" use:tilt={{ max: 15, speed: 400, glare: true, "max-glare": 0.2 }}>
+    <img class='w-20 h-20 object-contain mb-4 bg-dark/85 dark:bg-white/15 rounded-lg p-2' src={card.imgUrl} alt={card.imgAlt} />
+    <h2 class='text-xl font-bold text-black dark:text-white mb-1'>{card.Title}</h2>
+    <h3 class="pb-2 text-lg text-black/85 dark:text-white/85 font-medium">{card.Company}</h3>
+    <hr class='dark:invert' />
     <div class="CardDiv">
-        <div class="IconDiv"><Ico name='Location' /></div>
-        <h4>{card.Location}</h4>
+        <div class="IconDiv invert dark:invert-0"><Ico name='Location' /></div>
+        <h4 class='text-black/75 dark:text-white/75'>{card.Location}</h4>
     </div>
-    <hr />
+    <hr class='dark:invert' />
     <div class="CardDiv">
-        <div class="IconDiv"><Ico name="Date" /></div>
-        <h4>{card.Date}</h4>
+        <div class="IconDiv invert dark:invert-0"><Ico name="Date" /></div>
+        <h4 class='text-black/75 dark:text-white/75'>{card.Date}</h4>
     </div>
-    <hr />
+    <hr class='dark:invert' />
     <div class="CardDiv">
-        <div class="IconDiv"><Ico name='Time' /></div>
-        <h4>{card.Duration}</h4>
+        <div class="IconDiv invert dark:invert-0"><Ico name='Time' /></div>
+        <h4 class='text-black/75 dark:text-white/75'>{card.Duration}</h4>
     </div>
-    <hr />
-    <div class="CardDiv px-3 text-justify">
-        <p class='font-4'>{card.Details}</p>
+    <hr class='dark:invert' />
+    <div class="CardDiv flex shrink-1 grow-1 px-3 text-justify">
+        <p class='font-4 text-sm text-black/80 dark:text-white/60 leading-relaxed'>{card.Details}</p>
     </div>
-    <hr />
-    <div class="LastDiv">
-        <a class="ButtonCard Tooltip" href={card.CertificateUrl} data-title='Certificate' target='_blank'><Ico name='Certificate' /></a>
+    <hr class='dark:invert' />
+    <div class="flex pt-4 justify-end">
+        <a class="Tooltip rounded-3 no-underline cursor-pointer w-13 h-10 py-2 flex items-center justify-center transition-all duration-300 ease bg-black/80 dark:bg-white/5 dark:hover:shadow-[0_0_10px_white/30] hover:-translate-y-0.5" href={card.CertificateUrl} data-title='Certificate' target='_blank'><Ico name='Certificate' /></a>
     </div>
 </div>
 {/each}
 
 <style lang="scss">
     .CardEdu {
-        border-radius: 12px;
         box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-        @apply h-auto w-[80vw] md:w-[38vw] lg:w-[38vw] px-4 py-4 lg:px-6 lg:py-6 bg-[#0a0a0a] bg-opacity-70 backdrop-blur-md border border-white/10;
         transition: transform 0.3s ease, box-shadow 0.3s ease, background 0.3s ease;
 
         &:hover {
-            background: rgba(255, 255, 255, 0.03);
+            // background: rgba(255, 255, 255, 0.03);
             box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
         }
 
@@ -63,67 +61,15 @@
         }
 
         .IconDiv {
-            @apply w-5 h-5 pr-3 text-white/70 flex items-center justify-center;
-        }
-
-        .Logo {
-            @apply w-20 h-20 object-contain mb-4 bg-white/5 rounded-lg p-2;
-        }
-
-        h2 {
-            @apply text-xl font-bold text-white mb-1;
-        }
-
-        h3 {
-            @apply text-lg text-white/80 font-medium;
+            @apply w-5 h-5 mr-3 text-white/70 flex items-center justify-center;
         }
 
         h4 {
-            @apply text-sm text-white/70 font-medium m-0;
-        }
-
-        p {
-            @apply text-sm text-white/60 leading-relaxed;
+            @apply text-sm font-medium m-0;
         }
 
         hr {
-            border: none;
-            border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-            @apply my-2;
-        }
-    }
-
-    .Card1 {
-        @apply self-center md:self-start lg:self-start;
-    }
-
-    .Card2 {
-        @apply self-center md:self-end lg:self-end;
-    }
-
-    .LastDiv {
-        @apply flex pt-4 justify-end;
-    }
-
-    .ButtonCard {
-        border: 1px solid rgba(255, 255, 255, 0.2);
-        border-radius: 8px;
-        fill: #fff;
-        transition: all 0.3s ease;
-        text-decoration: none;
-        cursor: pointer;
-        @apply w-10 h-10 flex items-center justify-center bg-white/5;
-
-        &:hover {
-            background: rgba(255, 255, 255, 0.2);
-            box-shadow: 0 0 10px rgba(255,255,255,0.2);
-            transform: translateY(-2px);
-        }
-
-        &:active {
-            background-color: #fff;
-            fill: #000;
-            transform: translateY(0);
+            @apply border-none border-b-[.2px] border-black/20 border-b-solid;
         }
     }
 </style>
