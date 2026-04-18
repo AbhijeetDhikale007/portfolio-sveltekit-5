e594a5d3dd9c5b5a1f853f4b5100955dae052e24
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-18 14:04:56 +0530

    Minor Improvements

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,34 +1,18 @@
 <script lang='ts'>
     import { fly } from 'svelte/transition'
     import { cubicIn } from 'svelte/easing'
-    import Heading from '$components/Header.svelte';
+    import Heading from '$components/Heading.svelte';
     import Card from '$components/ExperienceCard.svelte';
 </script>
 
-<div class='Experience' in:fly={{ easing: cubicIn, y: 100, duration: 1200 }}>
+<div class='relative flex flex-col mt-5' in:fly={{ easing: cubicIn, y: 100, duration: 1200 }}>
     <Heading HeadingTitle='Experience' />
-    <div class='EduDivWrap'>
-        <div class='HR'></div>
-        <div class="EduDiv">
+    <div class='relative flex mt-10 mb-20'>
+        <div
+            class='border-1 border-white/20 absolute left-1/2 -translate-x-1/2 top-0 bottom-0 h-full hidden md:block z-[-1]'
+        ></div>
+        <div class='w-[88vw] mx-auto h-fit flex flex-col shrink-1 gap-y-20'>
             <Card />
         </div>
     </div>
 </div>
-
-<style lang='scss'>
-    .Experience {
-        .EduDivWrap {
-            @apply relative flex mt-10 mb-20;
-        }
-
-        .EduDiv {
-            @apply w-[88vw] mx-auto h-fit;
-            @apply flex flex-col shrink-1 gap-y-20;
-        }
-
-        .HR {
-            border-left: 1px solid rgba(255, 255, 255, 0.2);
-            @apply absolute left-1/2 -translate-x-1/2 top-0 bottom-0 h-full hidden md:block z-[-1];
-        }
-    }
-</style>
