7cfad5957070113ed3e7183b3769f71c66ed7302
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-20 14:33:14 +0530

    Minor Adjustment

--- src/data/Education.ts
+++ src/data/Education.ts
@@ -10,9 +10,9 @@ type Cards = {
 }[]
 
 export let Education: Cards = [
-    { CertificateUrl: 'Documents/SSC-Marksheet.pdf', imgUrl: 'Images/School.png', imgAlt: 'School', Degree: '10th SSC - Semi English', Name: 'New English School', Location: 'Adgaon, Nashik, Maharashtra', Date: '2018', Duration: '1 Year'},
-    { CertificateUrl: 'Documents/ITI-ConsolidatedMarksheet_R180827051938.pdf', imgUrl: 'Images/MVP.png', imgAlt: 'MVP', Degree: 'Industrial Training Institute', Name: `NDMVP Samaj's ITI`, Location: 'Nashik, Maharashtra', Date: 'Aug 2018 - May 2021', Duration: '2 Year' },
-    { CertificateUrl: 'Documents/Electrical-Diploma-Final-Sem-Marsheet.pdf', imgUrl: 'Images/MET.png', imgAlt: 'MET', Degree: 'Diploma - Electrical', Name: 'Met Bhujbal Knowledge City', Location: 'Adgaon, Nashik, Maharashtra', Date: 'Oct 2021 - Jun 2023', Duration: '2 Year' },
+    { CertificateUrl: '', imgUrl: 'Images/YCMOU.png', imgAlt: 'Certificate', Degree: 'Bachelor of Computer Application', Name: 'YCMOU', Location: 'Nashik, Maharashtra', Date: 'Sep 2023 - Present', Duration: 'Appear' },
     { CertificateUrl: 'Documents/Abhijeet Dhikale-FSD-Java-Certificate.pdf', imgUrl: 'Images/Seed.png', imgAlt: 'Java Certificate', Degree: 'Full Stack Developer - Java Plus', Name: 'Seed Infotech', Location: 'Nashik, Maharashtra', Date: 'Nov 2023 - Oct 2024', Duration: '6 Months' },
-    { CertificateUrl: '', imgUrl: 'Images/YCMOU.png', imgAlt: 'Certificate', Degree: 'Bachelor of Computer Application', Name: 'YCMOU', Location: 'Nashik, Maharashtra', Date: 'Sep 2023 - Present', Duration: 'Appear' }
+    { CertificateUrl: 'Documents/Electrical-Diploma-Final-Sem-Marsheet.pdf', imgUrl: 'Images/MET.png', imgAlt: 'MET', Degree: 'Diploma - Electrical', Name: 'Met Bhujbal Knowledge City', Location: 'Adgaon, Nashik, Maharashtra', Date: 'Oct 2021 - Jun 2023', Duration: '2 Year' },
+    { CertificateUrl: 'Documents/ITI-ConsolidatedMarksheet_R180827051938.pdf', imgUrl: 'Images/MVP.png', imgAlt: 'MVP', Degree: 'Industrial Training Institute', Name: `NDMVP Samaj's ITI`, Location: 'Nashik, Maharashtra', Date: 'Aug 2018 - May 2021', Duration: '2 Year' },
+    { CertificateUrl: 'Documents/SSC-Marksheet.pdf', imgUrl: 'Images/School.png', imgAlt: 'School', Degree: '10th SSC - Semi English', Name: 'New English School', Location: 'Adgaon, Nashik, Maharashtra', Date: '2018', Duration: '1 Year'}
 ]
