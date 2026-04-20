9c8ce862730307197d5783a14864ed384327001d
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-20 13:32:30 +0530

    Removed: Unsused Property

--- src/data/Experience.ts
+++ src/data/Experience.ts
@@ -1,5 +1,4 @@
 type Cards = {
-    CardClass: string,
     CertificateUrl: string,
     imgUrl: string,
     imgAlt: string,
@@ -12,6 +11,6 @@ type Cards = {
 }[]
 
 export let Experience: Cards = [
-    { CardClass: 'CardEdu Card1', CertificateUrl: 'Documents/Internship - Calanjiyam Consultancy.pdf', imgUrl: 'Images/Calanjiyam.png', imgAlt: 'Calanjiyam Consultancies and Technologies', Title: 'Full stack Web application Development Intern', Company: 'Calanjiyam Consultancies and Technologies', Location: 'Erode, Tamil Nadu - 638455', Date: '3 Jan 2025 - 5 Mar 2025', Duration: '8 Weeks', Details: 'An internship on Full stack Web application Development using HTML, CSS, JavaScript, jQuery, Ajax, PHP, and MySQL at Calanjiyam Consultancies and Technologies (8 Weeks). The internship was centred around the following project ‘E - learning platform’.' },
-    // { CardClass: 'CardEdu Card2', CertificateUrl: 'Documents/ITI-ConsolidatedMarksheet_R180827051938.pdf', imgUrl: 'assets/Images/MVP.png', imgAlt: 'MVP', Title: 'Industrial Training Institute', Company: `NDMVP Samaj's ITI`, Location: 'Nashik, Maharashtra', Date: 'Aug 2018 - May 2021', Duration: '2 Year', Details: '' },
+    { CertificateUrl: 'Documents/Internship - Calanjiyam Consultancy.pdf', imgUrl: 'Images/Calanjiyam.png', imgAlt: 'Calanjiyam Consultancies and Technologies', Title: 'Full stack Web application Development Intern', Company: 'Calanjiyam Consultancies and Technologies', Location: 'Erode, Tamil Nadu - 638455', Date: '3 Jan 2025 - 5 Mar 2025', Duration: '8 Weeks', Details: 'An internship on Full stack Web application Development using HTML, CSS, JavaScript, jQuery, Ajax, PHP, and MySQL at Calanjiyam Consultancies and Technologies (8 Weeks). The internship was centred around the following project ‘E - learning platform’.' },
+    // { CertificateUrl: 'Documents/ITI-ConsolidatedMarksheet_R180827051938.pdf', imgUrl: 'assets/Images/MVP.png', imgAlt: 'MVP', Title: 'Industrial Training Institute', Company: `NDMVP Samaj's ITI`, Location: 'Nashik, Maharashtra', Date: 'Aug 2018 - May 2021', Duration: '2 Year', Details: '' },
 ]
