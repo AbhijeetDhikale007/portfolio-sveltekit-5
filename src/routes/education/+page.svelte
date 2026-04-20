83238e9425e0daea0f0f7181525d395c78e20231
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-20 13:38:14 +0530

    Created: Education Page

---
+++ src/routes/education/+page.svelte
@@ -0,0 +1,35 @@
+<script lang='ts'>
+    import { fly } from 'svelte/transition'
+    import { cubicIn } from 'svelte/easing'
+    import Heading from '$components/Heading.svelte';
+    import { EducationCard as Card } from '$components/EducationCard.svelte';
+</script>
+
+<div class='Education'  in:fly={{ easing: cubicIn, y: 100, duration: 1200 }}>
+    <Heading HeadingTitle='Education' />
+    <div class='EduDivWrap'>
+        <div class='HR'></div>
+        <div class="EduDiv">
+            <Card />
+        </div>
+    </div>
+</div>
+
+<style lang='scss'>
+    .Education {
+        .EduDivWrap {
+            @apply relative flex mt-10 mb-20;
+        }
+
+        .EduDiv {
+            @apply w-[88vw] h-fit;
+            @apply flex flex-col shrink-1 gap-y-20;
+        }
+
+        .HR {
+            border-left: .1px solid rgb(255, 255, 255, 0.4);
+            @apply justify-self-center self-center w-0 h-100% absolute inset-0 ml-50% mr-50%;
+            @apply hidden md:block lg:block xl:block 2xl:block z--1 shrink-1 grow-1;
+        }
+    }
+</style>
