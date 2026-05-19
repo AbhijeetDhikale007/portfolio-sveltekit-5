d6431ad6288a94a2a667ca272ccd3aa395100332
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-05-19 12:48:59 +0530

    Added: hover:z-1 To All Skill Cards

--- src/lib/components/SkillsCards.svelte
+++ src/lib/components/SkillsCards.svelte
@@ -32,7 +32,7 @@
     }
 </script>
 
-<a href="/skills/{skill.slug}" class='Card border-none no-underline rounded-5 bg-black dark:bg-white flex grow-1 shrink-1 items-center justify-between overflow-hidden h-19 min-h-14 max-h-22 min-w-[72vw] w-[76vw] max-w-[84vw] md:max-h-22 md:h-19 md:min-h-16 md:max-w-[42vw] md:min-w-[33vw] md:w-[36vw] lg:min-w-68 lg:max-w-80 lg:w-76 lg:min-h-18 lg:max-h-19 xl:w-[24vw] xl:min-w-[22vw] xl:max-w-[24vw] 2xl:w-88 2xl:min-w-86 2xl:max-w-90 2xl:h-24 2xl:min-h-22' use:tilt>
+<a href="/skills/{skill.slug}" class='Card border-none no-underline rounded-5 bg-black hover:z-1 dark:bg-white flex grow-1 shrink-1 items-center justify-between overflow-hidden h-19 min-h-14 max-h-22 min-w-[72vw] w-[76vw] max-w-[84vw] md:max-h-22 md:h-19 md:min-h-16 md:max-w-[42vw] md:min-w-[33vw] md:w-[36vw] lg:min-w-68 lg:max-w-80 lg:w-76 lg:min-h-18 lg:max-h-19 xl:w-[24vw] xl:min-w-[22vw] xl:max-w-[24vw] 2xl:w-88 2xl:min-w-86 2xl:max-w-90 2xl:h-24 2xl:min-h-22' use:tilt>
     <p class='px-6 text-white dark:text-black'>{skill.name}</p>
     <Ico class='{skill.class} w-fit' name={skill.SVG} />
 </a>
