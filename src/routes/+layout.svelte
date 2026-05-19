bd33b2964c14b891fea2ada09ef9826b204c06ab
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-05-19 13:51:02 +0530

    Added: Meta Tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,39 @@
 
 <svelte:head>
 	<link rel="icon" href={Icon} />
+	<meta
+		name="description"
+		content="Aspiring Software Engineer passionate about building scalable and efficient web applications. Skilled in TypeScript, SvelteKit, and modern web technologies. Committed to continuous learning and creating impactful digital solutions."
+	/>
+	<meta
+		name="keywords"
+		content="Aspiring Software Engineer, Software Engineer, Web Applications, TypeScript, SvelteKit, Modern Web Technologies, Digital Solutions, Abhijeet Dhikale, Portfolio, SvelteKit"
+	/>
+	<meta
+		property="og:title"
+		content="Abhijeet Dhikale | Portfolio"
+	/>
+	<meta
+		property="og:description"
+		content="Aspiring Software Engineer passionate about building scalable and efficient web applications. Skilled in TypeScript, SvelteKit, and modern web technologies. Committed to continuous learning and creating impactful digital solutions."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://abhijeetdhikale007.github.io" />
+	<meta property="og:image" content="https://abhijeetdhikale007.github.io/A.png" />
+	<meta
+		property="og:description"
+		content="Aspiring Software Engineer passionate about building scalable and efficient web applications. Skilled in TypeScript, SvelteKit, and modern web technologies. Committed to continuous learning and creating impactful digital solutions."
+	/>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta
+		name="twitter:title"
+		content="Abhijeet Dhikale | Portfolio"
+	/>
+	<meta
+		name="twitter:description"
+		content="Aspiring Software Engineer passionate about building scalable and efficient web applications. Skilled in TypeScript, SvelteKit, and modern web technologies. Committed to continuous learning and creating impactful digital solutions."
+	/>
+	<meta name="twitter:image" content="https://abhijeetdhikale007.github.io/A.png" />
 </svelte:head>
 
 <Header />
