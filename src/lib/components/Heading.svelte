0db59dfdaf50821c3057c2d6826ea33325dcfca0
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-05-26 18:57:38 +0530

    Added: Selection:text Background Color

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -3,7 +3,7 @@
 </script>
 
 	<h1
-	class="heading text-center mt-18 mb-3 text-20 text-transperent font-extrabold font-Arsenal text-[#adada9] bg-gradient-to-t from-[#adada9] to-black dark:bg-gradient-to-t from-[#adada9] dark:to-white bg-clip-text text-transparent"
+	class="heading text-center mt-18 mb-3 text-20 text-transperent font-extrabold font-Arsenal text-[#adada9] bg-gradient-to-t from-[#adada9] to-black dark:bg-gradient-to-t from-[#adada9] dark:to-white bg-clip-text text-transparent selection:bg-purple/45"
 	>
 	{HeadingTitle}
 	</h1>
